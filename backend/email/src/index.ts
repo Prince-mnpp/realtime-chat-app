e8dbbcbcf4e2fe8797c655080926b8e8dcbd8c89
Prince-mmpp <princerajput4537@gmail.com>
2026-09-06 18:26:38 +0530

    email server created

--- backend/email/src/index.ts
+++ backend/email/src/index.ts
@@ -2,3 +2,8 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT ?? 5001;
+
+app.listen(prompt, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+})
